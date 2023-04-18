6adbe0d10ed3a22283da7f8e5d971cffe0372c68
Bill <jamiepwheeler@gmail.com>
2023-04-18 23:28:50 +0100

    Add quiz yml upload feature

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const config = require("./config.json");
 const secret = require("./secret.json");
+const yaml = require('js-yaml');
+const https = require('https');
+const fs = require('fs');
 
 const { EmbedBuilder } = require('discord.js');
 const { Client, GatewayIntentBits } = require('discord.js');
@@ -49,9 +52,48 @@ client.on("interactionCreate", async message => {
 			embed.addFields({ name: "c: answer 3: ", value: "1 points", inline: true});
 			embed.addFields({ name: "d: answer 4: ", value: "0 points", inline: true});
 			
-            //embed.setDescription(`<@${data.userId}> ${data.points} points`);
+            
         
         message.reply({ embeds: [embed] });
+
+    }
+
+    if (message.commandName === "uploadquiz"){
+        let quizAttach = message.options.getAttachment("ymlfile");
+        let quizName = message.options.getString("quizname");
+
+        if(fs.existsSync(message.user.id.toString().concat('.yml'))){   // Set this to quiz name
+            console.log("it exists")
+            const fileContents = fs.readFileSync(message.user.id.toString().concat('.yml'), 'utf-8');
+           
+            const doc = yaml.load(fileContents);
+            //doc.newKey = 'testval:test1';
+            
+            
+            //doc.quiz.name = 'quizy'; 
+            const yamlStr = yaml.dump(data);
+            fs.writeFileSync('example.yml', yamlStr, 'utf8');
+            //console.log(doc['actions']['type'])
+            message.reply("test");
+        }else{
+            console.log("Quiz does not a duplicate. Donloading...")
+            
+            const file = fs.createWriteStream(quizName + ".yml");
+            const request = https.get(quizAttach.url, function(response) {
+                response.pipe(file);
+
+                file.on("finish", () => {
+                    file.close();
+                    console.log(quizName + ".yml, has been downloaded")
+                })
+            });
+                                         
+            
+            message.reply("Your quiz " + quizName + " was successfully uploaded!");
+           
+        };
+
+        
 
     }
 });
--- slashCommands.js
+++ slashCommands.js
@@ -7,9 +7,32 @@ const { token } = require("./secret.json");
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
-	new SlashCommandBuilder().setName('embedtest').setDescription('tets'),								
-
-									
+	new SlashCommandBuilder().setName('embedtest').setDescription('tets'),
+	new SlashCommandBuilder().setName('uploadquiz').setDescription('Upload a quiz file!')
+							.addAttachmentOption(attach => 
+								attach.setName("ymlfile")
+									.setRequired(true)
+									.setDescription("Upload a .yml file of your custom quiz!"))
+							.addStringOption(title =>
+								title.setName("quizname")
+									.setRequired(true)
+									.setDescription("The title of your quiz")),
+	/*								
+	new SlashCommandBuilder().setName('createquiz').setDescription('Create a quiz!')	
+							.addStringOption(quizTitle =>
+								quizTitle.setName("title")
+									.setDescription("Title of your quiz")
+									.setRequired(true))
+							.addStringOption(quizDesc =>
+								quizDesc.setName("description")
+									.setDescription("Description of your quiz")
+									.setRequired(true))
+							.addNumberOption(quizNumOfQs =>
+								quizNumOfQs.setName("numberofquestions")
+									.setDescription("How many questions are in your quiz")
+									.setRequired(true)),
+									*/
+						
 ]
 	.map(command => command.toJSON());
 
